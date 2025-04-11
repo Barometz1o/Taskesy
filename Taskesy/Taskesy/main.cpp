eff5b876b2cac4b2e48018bb7f9ca66784a17461
Barometz1o <raulvelazquezgomez2@gmail.com>
2025-04-11 16:56:13 +0200

    First steps into doing the table system correctly

--- Taskesy/Taskesy/main.cpp
+++ Taskesy/Taskesy/main.cpp
@@ -607,6 +607,54 @@ int main(int, char**)
                     ImGui::TableSetColumnIndex(column);
                     ImGui::Text("Hello %d,%d", column, row);
                 }
+                for (int n = 0; n < IM_ARRAYSIZE(names); n++)
+                {
+                    ImGui::PushID(n);
+                    if ((n % numColumns) != 0)
+                        ImGui::SameLine();
+
+                    ImGui::Text("Column: ", n);
+                    ImGui::Button(names[n], ImVec2(boxSizeX, boxSizeY));
+
+                    // Our buttons are both drag sources and drag targets here!
+                    if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
+                    {
+                        // Set payload to carry the index of our item (could be anything)
+                        ImGui::SetDragDropPayload("DND_DEMO_CELL", &n, sizeof(int));
+
+                        // Display preview (could be anything, e.g. when dragging an image we could decide to display
+                        // the filename and a small preview of the image, etc.)
+                        if (mode == Mode_Copy) { ImGui::Text("Copy %s", names[n]); }
+                        if (mode == Mode_Move) { ImGui::Text("Move %s", names[n]); }
+                        if (mode == Mode_Swap) { ImGui::Text("Swap %s", names[n]); }
+                        ImGui::EndDragDropSource();
+                    }
+                    if (ImGui::BeginDragDropTarget())
+                    {
+                        if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("DND_DEMO_CELL"))
+                        {
+                            IM_ASSERT(payload->DataSize == sizeof(int));
+                            int payload_n = *(const int*)payload->Data;
+                            if (mode == Mode_Copy)
+                            {
+                                names[n] = names[payload_n];
+                            }
+                            if (mode == Mode_Move)
+                            {
+                                names[n] = names[payload_n];
+                                names[payload_n] = "";
+                            }
+                            if (mode == Mode_Swap)
+                            {
+                                const char* tmp = names[n];
+                                names[n] = names[payload_n];
+                                names[payload_n] = tmp;
+                            }
+                        }
+                        ImGui::EndDragDropTarget();
+                    }
+                    ImGui::PopID();
+                }
             }
             ImGui::EndTable();
         }
